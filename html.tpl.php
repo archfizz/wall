65813fec5a8b516ec7756df976416601703ab7bb
Adam Elsodaney <adam.elso@gmail.com>
2012-10-14 02:58:50 +0100

    Closes #1 - Using Drupal/PEAR Coding Style instead of PSR-2

--- html.tpl.php
+++ html.tpl.php
@@ -3,28 +3,28 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
+  <head>
+    <meta charset="utf-8">
 
-        <?php print $head; ?>
-        <title><?php print $head_title; ?> :: Michelle Baharier</title>
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
 
-        <meta name="description" content="Portfolio of Michelle Baharier">
-        <meta name="author" content="Michelle Baharier">
-        <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Portfolio of Michelle Baharier">
+    <meta name="author" content="Michelle Baharier">
+    <meta name="viewport" content="width=device-width">
 
-        <?php print $styles; ?>
-        
-        <?php /* Place favicon.ico and apple-touch-icon.png in the root directory */ ?>
+    <?php print $styles; ?>
+    
+    <?php /* Place favicon.ico and apple-touch-icon.png in the root directory */ ?>
 
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.6.1.min.js"></script>
-    </head>
-    <body class="<?php print $classes; ?>" <?php print $attributes;?>>
-        <?php print $page_top; ?>
-        <?php print $page; ?>
-        <?php print $page_bottom; ?>
-        <?php print $scripts; ?>
-    </body>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.6.1.min.js"></script>
+  </head>
+  <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+    <?php print $page_top; ?>
+    <?php print $page; ?>
+    <?php print $page_bottom; ?>
+    <?php print $scripts; ?>
+  </body>
 </html>
